6546d4c32286606543e9e03185a8978a830acc41
salomonbego34-art <salomonbego34@gmail.com>
2026-04-10 11:32:42 +0700

    Add files via upload

--- server/index.ts
+++ server/index.ts
@@ -113,3 +113,9 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
